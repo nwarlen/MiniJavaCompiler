0487bd38f87d155b5af62fb48e79954f6b8df2b8
nwarlen <warlen15@up.edu>
2015-03-21 22:52:08 -0700

    Begin work on Sem5. TODO: Fix visitStatementList

--- src/visitor/Sem5Visitor.java
+++ src/visitor/Sem5Visitor.java
@@ -1,5 +1,6 @@
 package visitor;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import syntaxtree.*;
 import errorMsg.*;
 
@@ -20,5 +21,58 @@ public class Sem5Visitor extends ASTvisitor {
 		conEval = new ConstEvalVisitor();
 		sawBreak = false;
 	}
+    
+    public Boolean visitAstNode(AstNode astNode) {
+        return Boolean.FALSE;
+    }
+    
+    public Boolean visitProgram(Program program) {
+        visitClassDeclList(program.classDecls);
+        return Boolean.FALSE;
+    }
+    
+    public Boolean visitClassDecl(ClassDecl classDecl) {
+        for (MethodDecl methodDecl : classDecl.methodTable.values()) {
+            visitMethodDecl(methodDecl);
+        }
+        return Boolean.FALSE;
+    }
+    
+    public Boolean visitBlock(Block block) {
+        return visitStatementList(block.stmts);
+        
+    }
+    
+    public Boolean visitBreak(Break breakStatement) {
+        this.sawBreak = true;
+        return Boolean.TRUE;
+    }
+    
+    //TODO: Read Email and check implementation
+    public Boolean visitStatementList(StatementList statementList) {
+        if (statementList.size() == 0) {
+            return Boolean.FALSE;
+        }
+        
+        for(int i=0; i<statementList.size();i++) {
+            if (i != (statementList.size() - 1)) {
+                //if visiting the statement returns true report error
+                Statement stmt = statementList.get(i);
+                if (visitStatement(stmt).equals(Boolean.TRUE)) {
+                    this.errorMsg.error(stmt.pos, "Error: Unreachable Code Detected");
+                }
+            }
+        }
+        
+        return (Boolean)visitStatement(statementList.get(statementList.size() -1));
+    }
+    
+    public Boolean visitMethodDeclVoid(MethodDeclVoid methodDeclVoid) {
+        this.sawBreak = false;
+        
+        visitStatementList(methodDeclVoid.stmts);
+        
+        return Boolean.FALSE;
+    }
 }
 	
